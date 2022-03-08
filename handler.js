f599ce87ede125976353c074458acff763f0195c
Quentin Lintz <quentinlintz@outlook.com>
2022-03-07 23:09:22 -0500

    Add Datamuse and rename endpoint

--- handler.js
+++ handler.js
@@ -1,22 +1,17 @@
-const serverless = require("serverless-http");
-const express = require("express");
+const serverless = require('serverless-http');
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res, next) => {
+// Get the current daily challenge object from the database
+app.get('/daily', (req, res, next) => {
   return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
+    message: 'Hello from root!',
   });
 });
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    error: 'Not Found',
   });
 });
 
