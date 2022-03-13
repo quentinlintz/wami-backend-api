1fa9a81fb2c9bffc80409ec2d692693cad14ce56
Quentin Lintz <98997219+quentinlintz@users.noreply.github.com>
2022-03-13 18:03:16 -0400

    Add DynamoDB and create today endpoint (#3)
    * Refactor functions
    
    * Refactor serverless.yml and add params to challengeData in the /new endpoint
    
    * Add conditional request to creating a new daily challenge
    
    * Get today's challenge based on the date

---
+++ src/functions/challenges/new.js
@@ -0,0 +1,70 @@
+const serverless = require('serverless-http');
+const AWS = require('aws-sdk');
+const express = require('express');
+const format = require('date-format');
+const cors = require('cors');
+const axios = require('axios');
+const _ = require('lodash');
+const nouns = require('./nouns');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const app = express();
+
+const TOTAL_HINTS = 8;
+
+const CORS_OPTIONS = {
+  origin: process.env.ALLOWED_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+const options = (entry) => {
+  return {
+    method: 'GET',
+    url: 'https://twinword-word-associations-v1.p.rapidapi.com/associations/',
+    params: { entry },
+    headers: {
+      'x-rapidapi-host': 'twinword-word-associations-v1.p.rapidapi.com',
+      'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+    },
+  };
+};
+
+app.use(cors(CORS_OPTIONS));
+
+// Get a new, random challenge to tomorrow
+app.post('/new', async (req, res, next) => {
+  // Get tomorrow's date in the form of YYYYMMDD as an integer
+  let tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const date = parseInt(format('yyyyMMdd', tomorrow));
+
+  // Get one random object word
+  const answer = _.sampleSize(nouns)[0];
+
+  // Request a set of hints and take a subset
+  const response = await axios.request(options(answer));
+  const hints = _.sampleSize(response.data.associations_array, TOTAL_HINTS);
+
+  const challengeData = {
+    date,
+    answer,
+    hints,
+  };
+
+  try {
+    // Add the new challenge information to the table if one for tomorrow doesn't exist
+    await dynamodb
+      .put({
+        TableName: process.env.CHALLENGES_TABLE_NAME,
+        Item: challengeData,
+        ConditionExpression: 'attribute_not_exists(#challenge_day)',
+        ExpressionAttributeNames: { '#challenge_day': 'date' },
+      })
+      .promise();
+
+    return res.status(200).json(challengeData);
+  } catch (error) {
+    return res.status(400).json({ error: error.toString() });
+  }
+});
+
+module.exports.handler = serverless(app);
