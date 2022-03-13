1e6c3143934bf46d2d27fc7da202af53f1b7a4f2
Quentin Lintz <quentinlintz@outlook.com>
2022-03-13 16:43:29 -0400

    Refactor serverless.yml and add params to challengeData in the /new endpoint

--- src/functions/challenges/new.js
+++ src/functions/challenges/new.js
@@ -1,12 +1,12 @@
 const serverless = require('serverless-http');
 const AWS = require('aws-sdk');
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const _ = require('lodash');
 const nouns = require('./nouns');
-const client = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 const app = express();
 
 const TOTAL_HINTS = 8;
@@ -30,26 +30,50 @@ const options = (entry) => {
 
 app.use(cors(CORS_OPTIONS));
 
-// Get a new, random challenge
-app.get('/new', async (req, res, next) => {
+// Get a new, random challenge to tomorrow
+app.post('/new', async (req, res, next) => {
+  // Check if the challenge for tomorrow has been already created
+  // await client;
+
+  // Get tomorrow's date
+  let today = new Date();
+  let tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  tomorrow.setMonth(today.getMonth() + 1);
+  let date =
+    tomorrow.getMonth() +
+    '-' +
+    tomorrow.getDate() +
+    '-' +
+    tomorrow.getFullYear();
+
   // Get one random object word
   const answer = _.sampleSize(nouns)[0];
 
-  axios
-    .request(options(answer))
-    .then((response) => {
-      const hints = _.sampleSize(response.data.associations_array, TOTAL_HINTS);
+  // Request a set of hints and take a subset
+  try {
+    const response = await axios.request(options(answer));
+    const hints = _.sampleSize(response.data.associations_array, TOTAL_HINTS);
 
-      const challengeData = {
-        answer,
-        hints,
-      };
+    const challengeData = {
+      id: uuidv4(),
+      date,
+      answer,
+      hints,
+    };
 
-      return res.status(200).json(challengeData);
-    })
-    .catch((err) => {
-      return next(err);
-    });
+    // Add the new challenge information to the table
+    // await dynamodb
+    //   .put({
+    //     TableName: process.env.CHALLENGE_TABLE_NAME,
+    //     Item: challengeData,
+    //   })
+    //   .promise();
+
+    return res.status(200).json(challengeData);
+  } catch (error) {
+    return res.status(400).json({ error: error.toString() });
+  }
 });
 
 module.exports.handler = serverless(app);
